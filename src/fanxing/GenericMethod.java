baa2bb28b55db36aa49804aa434abc2f61d18ace
wangzhiguo <17614430417@163.com>
2020-03-16 09:20:58 +0800

    generic

---
+++ src/fanxing/GenericMethod.java
@@ -0,0 +1,24 @@
+package fanxing;
+
+/**
+ *
+ * 泛型方法
+ * @author wangzhiguo
+ */
+public class GenericMethod {
+
+    public <T> void method1(T a, T b) {
+        System.out.println("a " + a);
+        System.out.println("b " + b);
+    }
+
+    public static <T> void method2(T a, T b) {
+        System.out.println("a " + a);
+        System.out.println("b " + b);
+    }
+
+    public static <T extends Comparable<T>> T getMin(T a, T b) {
+        return (a.compareTo(b) < 0) ? a : b;
+    }
+
+}
