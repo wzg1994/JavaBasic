037d52bc1d2ff3205c7395f7c8c54f7d09d3b239
wangzhiguo <17614430417@163.com>
2020-07-06 17:21:27 +0800

    链表习题

---
+++ src/algorithm/linkedList/LinkedList.java
@@ -0,0 +1,113 @@
+package algorithm.linkedList;
+
+/**
+ * 链表
+ * @author wangzhiguo
+ */
+public class LinkedList {
+
+    /**
+     * 206，141，21，19，876
+     * 单链表反转
+     * 链表中环的检测
+     * 两个有序的链表合并
+     * 删除链表倒数第n个结点
+     * 求链表的中间结点
+     */
+
+    public static void main(String[] args) {
+        Node head = new Node();
+        head.setVal(1);
+
+        Node node1 = new Node();
+        node1.setVal(2);
+        head.setNext(node1);
+
+        Node node2 = new Node();
+        node2.setVal(3);
+        node1.setNext(node2);
+
+        Node node3 = new Node();
+        node3.setVal(4);
+        node2.setNext(node3);
+
+        Node node4 = new Node();
+        node4.setVal(5);
+        node3.setNext(node4);
+        Node node = reverseList(head);
+        printElementByRecursive(node);
+    }
+
+    /**
+     * 1，2，3，4，5反转
+     */
+    public static Node reverseList(Node head) {
+
+        Node temp = null;
+
+        while (head != null) {
+            temp = head.next;
+            head.next = temp;
+        }
+
+        return null;
+    }
+
+    /**
+     * 通过循环
+     * 打印链表中每一个元素
+     */
+    public static Node printElementByWhile(Node head) {
+        while (head !=null) {
+            System.out.println(head.val);
+            head = head.next;
+        }
+
+        return null;
+    }
+
+    /**
+     * 通过递归
+     * 打印链表中每一个元素
+     */
+    public static Node printElementByRecursive(Node head) {
+        if (head == null) {
+            return null;
+        }
+
+        System.out.println(head.val);
+
+        if (head.next == null) {
+            return null;
+        }
+
+        Node node = printElementByRecursive(head.next);
+
+        return null;
+    }
+
+    static class Node{
+
+        int val;
+
+        Node next;
+
+        public int getVal() {
+            return val;
+        }
+
+        public void setVal(int val) {
+            this.val = val;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+    }
+
+
+}
