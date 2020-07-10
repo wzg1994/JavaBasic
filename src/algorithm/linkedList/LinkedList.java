7e2b8f7c4581b0e7055f2df25167c481f0e9c59d
wangzhiguo <17614430417@163.com>
2020-07-10 16:42:03 +0800

    链表习题

--- src/algorithm/linkedList/LinkedList.java
+++ src/algorithm/linkedList/LinkedList.java
@@ -34,7 +34,15 @@ public class LinkedList {
         ListNode listNode4 = new ListNode();
         listNode4.setVal(5);
         listNode3.setNext(listNode4);
-        ListNode listNode = removeNthFromEnd2(head, 2);
+
+
+        ListNode head2 = new ListNode();
+        head2.setVal(1);
+        ListNode listNode22 = new ListNode();
+        listNode22.setVal(4);
+        head2.setNext(listNode22);
+
+        ListNode listNode = mergeTwoLists2(head, head2);
 //        ListNode listNode = getMidNode2(head);
         printElementByRecursive(listNode);
     }
@@ -171,16 +179,49 @@ public class LinkedList {
      * 输出：1->1->2->3->4->4
      * 合并两个有序列表
      */
-    public ListNode mergeTwoLists1(ListNode l1, ListNode l2) {
-        if (l1 == null) {
-            return null;
+    public static ListNode mergeTwoLists1(ListNode l1, ListNode l2) {
+        ListNode prehead = new ListNode(-1);
+
+        ListNode prev = prehead;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
         }
 
-        l1.next = l2;
+        // 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
+        prev.next = l1 == null ? l2 : l1;
 
+        return prehead.next;
+    }
 
-
-        return null;
+    /**
+     * 输入：1->2->4, 1->3->4
+     * 输出：1->1->2->3->4->4
+     * 合并两个有序列表
+     */
+    public static ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        //当其中有一个链表为空时就返回
+        if (l1 == null) {
+            return l2;
+        }
+        else if (l2 == null) {
+            return l1;
+        }
+        //进行递归
+        else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists2(l1.next, l2);
+            return l1;
+        }
+        else {
+            l2.next = mergeTwoLists2(l1, l2.next);
+            return l2;
+        }
     }
 
     /**
