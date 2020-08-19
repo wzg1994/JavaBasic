378af5a6c5377ede9c0d12d79f9c5b2fc4e05571
wangzhiguo <17614430417@163.com>
2020-08-19 09:09:19 +0800

    二分查找实现平方根函数

--- src/algorithm/sort/SortDemo.java
+++ src/algorithm/sort/SortDemo.java
@@ -127,4 +127,35 @@ public class SortDemo {
         return result;
     }
 
+    /**
+     *   使用二分查找实现平方根函数，要求精确到小数点后6位
+     */
+    public float sqrt_search(float n){
+        float mid = 0.0f;
+        if(n < -1e-6){
+            // 小于0，抛异常
+            throw new IllegalArgumentException();
+        }else if(Math.abs(n) >= -1e-6 && Math.abs(n) <= 1e-6){
+            return mid;
+        }else{
+            // 逐次逼近，默认平方根的不会超过n的一半
+            float high = n / 2.0f;
+            float low = 0.0f;
+            while(Math.abs(high - low) > 1e-6){
+                // 首先找到中间值
+                mid = low + (high - low) / 2;
+                float tmp = mid * mid;
+                // 比较并更新 high和low
+                if((tmp - n) > 1e-6){
+                    high = mid;
+                }else if((tmp -n) < -1e-6){
+                    low = mid;
+                }else{
+                    return mid;
+                }
+            }
+        }
+        return mid;
+    }
+
 }
