8b8ff0a5e705ec05ba3e0ac422f326a5abcc8ea7
wangzhiguo <17614430417@163.com>
2020-12-29 16:44:41 +0800

    test

---
+++ src/main/java/test/Main.java
@@ -0,0 +1,38 @@
+package test;
+
+/**
+ * @author wangzhiguo
+ */
+public class Main  {
+
+    private static final SafeCount SAFE_COUNT = new SafeCount();
+
+    public static void main(String[] args) throws InterruptedException {
+
+        for (int i = 0; i < 4; i++) {
+            Thread thread = new Thread(() -> {
+                SAFE_COUNT.inc();
+                SAFE_COUNT.getI();
+            });
+            thread.start();
+        }
+    }
+
+//    public static void main(String[] args) {
+//        Thread th1 = new Thread(new Main(), "th1");
+//        Thread th2 = new Thread(new Main(), "th2");
+//        Thread th3 = new Thread(new Main(), "th3");
+//        Thread th4 = new Thread(new Main(), "th4");
+//        th1.start();
+//        th2.start();
+//        th3.start();
+//        th4.start();
+//    }
+//
+//    @Override
+//    public void run() {
+//        SAFE_COUNT.inc();
+//        int i = SAFE_COUNT.getI();
+////        System.out.println(i);
+//    }
+}
---
+++ src/main/java/test/SafeCount.java
@@ -0,0 +1,21 @@
+package test;
+
+/**
+ * @author wangzhiguo
+ */
+public class SafeCount {
+
+    private int i;
+
+    public synchronized int getI() {
+        System.out.println(Thread.currentThread().getName() + " -> get...");
+        System.out.println(i);
+        return i;
+    }
+
+    public synchronized void inc() {
+        System.out.println(Thread.currentThread().getName() + " -> inc...");
+        i++;
+//        System.out.println(getI());
+    }
+}
